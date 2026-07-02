96023d5bbfc2980d10f66776ac3b6ef1e42f6515
pepesr-dev <180779764+pepesr-dev@users.noreply.github.com>
2026-07-02 18:25:26 +0200

    registro de usuarios listo

--- src/database/migrations/2026_06_27_170202_add_username_to_users_table.php
+++ src/database/migrations/2026_06_27_170202_add_username_to_users_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //Coloca la columna después del id
-            $table->string('user_name')->unique()->after('id');
+            $table->string('username')->unique()->after('id');
         });
     }
 
